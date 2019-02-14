9c5ebfec1865235f69e10727e680a2c033156dfd
Bryce Sheffield <btsheffi@nuskin.com>
2019-02-14 09:49:07 -0700

    updated for async

--- test/test.js
+++ test/test.js
@@ -1,12 +1,10 @@
 const assert = require('assert');
-const fetch = require("node-fetch");
 const config = require("config");
 
 describe('CreateGetItem', function() {
     this.timeout(5000);
     it('should attempt to create an Item and Retrieve an Item', function(done) {
-        config.get("NS")
-
+        config.get("NS_SERVICE")
 
             assert(true, true);
             done();
